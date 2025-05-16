e6fa77fb1093c30306d1e14e7f68b5e5d0219896
Islam Aref <125500794+islam-aref@users.noreply.github.com>
2025-05-16 20:30:30 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -191,7 +191,7 @@ function App() {
         <div className="video-container">
           <h3>Discover our facilities</h3>
           <iframe
-            src="https://drive.google.com/file/d/1Sgm8R_dVJ_bCnOQ_X4Mmi9SYwGykkPKW/view"
+            src="https://drive.google.com/file/d/1Sgm8R_dVJ_bCnOQ_X4Mmi9SYwGykkPKW/preview"
             title="Our Story"
             allowFullScreen
           ></iframe>
